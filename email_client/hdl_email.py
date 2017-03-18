abce2b2d23a04e7b1c2e114453a22c706264b73a
cloud_sophier <d15821917291@gmail.com>
2017-03-18 14:23:32 +0800

    Update and rename new2.py to hdl_email.py

--- email_client/hdl_email.py
+++ email_client/hdl_email.py
@@ -6,7 +6,6 @@
 	程序类型：OOP
 	库：poplib、smtplib、email 前两个分别负责收发邮件，最后一个负责解析和构造邮件
 	实现的功能：登录邮箱，查看邮件总数，查看指定邮件，邮件发送\转发\删除等
-	参考资料：廖大《leanring python3》教程；poplib、smtplib、email官方文档；MIME邮件格式分析的博文（blog.csdn.net/xjbclz/article/details/51912725）
 """
 import poplib
 from email.parser import Parser
