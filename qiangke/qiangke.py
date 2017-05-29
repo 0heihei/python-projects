ab3f34176942b644271ae083205d5f6df7398ea0
clouduan <clouduan@gmail.com>
2017-05-29 13:41:16 +0800

    abandon

--- qiangke/qiangke.py
+++ qiangke/qiangke.py
@@ -5,34 +5,59 @@ import urllib.parse
 import os,re
 from bs4 import BeautifulSoup
 
-# 开始构造表单
+headers = {
+		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+		'Accept-Encoding': 'gzip, deflate, br',
+		'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4,ja;q=0.2,zh-TW;q=0.2',
+		'Connection': 'keep-alive',
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Upgrade-Insecure-Requests': '1',
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+	}
 s = requests.Session()
-html = s.get('http://electsys.sjtu.edu.cn/edu/login.aspx').content.decode('utf8')
-sid = re.findall('name="sid" value="(.+?)"', html)[0]     # 有待用美丽汤改写
-returl = re.findall('name="returl" value="(.+?)"', html)[0]
-se = re.findall('name="se" value="(.+?)"', html)[0]
-# usr=input('请输入用户名：')
-# pwd=input('请输入密码：')
-usr = 'duanyunzhi'
-pwd = 'sjtu184052'
-vfcode = s.get('https://jaccount.sjtu.edu.cn/jaccount/captcha').content
-with open('vf_code.png', 'wb') as f:
-	f.write(vfcode)
-os.startfile("vf_code.png")
-captcha = input("输入验证码：")
-login_data = {
-	'sid': sid,
-	'returl': returl,
-	'v': '',
-	'user': usr,
-	'pass': pwd,
-	'captcha': captcha
-}
-login_data = urllib.parse.urlencode(login_data).encode('utf8')
-# 登录
-login_url = 'https://jaccount.sjtu.edu.cn/jaccount/ulogin'
-login = s.post(login_url, data=login_data, )
+s.headers=headers
+req = s.get('http://electsys.sjtu.edu.cn/edu/')
+cookies_dict = {
+	'ASP.NET_SessionId': req.cookies['ASP.NET_SessionId'],
+	'mail_test_cookie': req.cookies['mail_test_cookie']}
+cookies = requests.utils.cookiejar_from_dict(cookies_dict)
 
+def login():
+	req=s.get('http://electsys.sjtu.edu.cn/edu/login.aspx',allow_redirects=False)
+	req2=s.get(req.headers['Location'],allow_redirects=False)
+	req3=s.get(req2.headers['Location'])
+	html = req3.content.decode('utf8')
+	sid = re.findall('name="sid" value="(.+?)"', html)[0]  # 有待用美丽汤改写
+	returl = re.findall('name="returl" value="(.+?)"', html)[0]
+	se = re.findall('name="se" value="(.+?)"', html)[0]
+	# usr=input('请输入用户名：')
+	# pwd=input('请输入密码：')
+	usr = 'duanyunzhi'
+	pwd = 'sjtu184052'
+	vfcode = s.get('https://jaccount.sjtu.edu.cn/jaccount/captcha').content
+	with open('vf_code.png', 'wb') as f:
+		f.write(vfcode)
+	os.startfile("vf_code.png")
+	captcha = input("输入验证码：")
+	#构造表单
+	login_data = {
+		'sid': sid,
+		'returl': returl,
+		'v': '',
+		'user': usr,
+		'pass': pwd,
+		'captcha': captcha
+	}
+	login_data = urllib.parse.urlencode(login_data).encode('utf8')
+	# 登录
+	login_url = 'https://jaccount.sjtu.edu.cn/jaccount/ulogin'
+	login = s.post(login_url, data=login_data,allow_redirects=False)
+	print(login.headers)
+
+login()
+
+
+'''
 post_url = 'http://electsys.sjtu.edu.cn/edu/student/elect/electwarning.aspx?xklc=1'
 post_data = {
 	'__VIEWSTATE': '/wEPDwUKLTQzMTk1OTAyMg8WAh4EeGtsYwUBMWQYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgEFCUNoZWNrQm94MWNhFMcb/llVDn1pEFCfZhKhz0Iu',
@@ -43,9 +68,7 @@ post_data = {
 }
 post_data = urllib.parse.urlencode(post_data).encode('utf8')
 #此处必须要更新cookies，不然通不过验证
-cookies_dict = {
-	'ASP.NET_SessionId': '2t4hi0552wfesp45u1tbjout',
-	'mail_test_cookie': 'IGIBHIAK'}
+
 cookies = requests.utils.cookiejar_from_dict(cookies_dict)
 s.cookies = cookies   #因为cookie的原因，必须保持网页端登录状态这里才可以登录？
 get_courses = s.post(post_url, post_data)
@@ -68,3 +91,4 @@ data={
 data=urllib.parse.urlencode(data).encode('utf8')
 post=s.post('http://electsys.sjtu.edu.cn/edu/student/elect/speltyRequiredCourse.aspx',data)  #提交查看该课程
 print(post.headers)  #获取response的headers中的location，凭此get到该课程的地址
+'''
